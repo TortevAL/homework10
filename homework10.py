9f72647c0f8723efb9ed9df0b7c166880b6efc42
TortevAL <165218904+TortevAL@users.noreply.github.com>
2024-04-17 16:54:47 +0300

    Update homework10.py

--- homework10.py
+++ homework10.py
@@ -1,8 +1,25 @@
+# 1 Вариант
 def print_params(name):
-    print(name + ' T.L.')
+    print((name + ' T.L. ') * 2)
 
 print_params('Denis')
 print_params('Artem')
 print_params('Vova')
-print_params('Job')
-print_params('Ivan')
+
+# 2 Вариант
+def print_params(name):
+    print(name + ' T.L. ')
+    print(name + ' T.L. ')
+
+print_params('Denis')
+print_params('Artem')
+print_params('Vova')
+
+# 3 Вариант
+def print_params(name):
+    for i in range(2):
+        print(name + ' T.L. ')
+
+print_params('Denis')
+print_params('Artem')
+print_params('Vova')
